fc7be8190a9224b74044b7663cd80dd51bf54f22
xiaolu2333 <2694977226@qq.com>
2023-01-03 17:48:58 +0800

    搭建管理后台：导航菜单之定义侧边栏菜单路由。

--- src/router/index.js
+++ src/router/index.js
@@ -6,6 +6,29 @@ const routes = [
   {
     path: "/",
     name: "Home", // 命名路由。这样我们就可以在任何组件中使用 $router.push({ name: 'Home' }) 来导航到这个路由，或者使用 <router-link :to="{ name: 'Home' }">Home</router-link> 来创建一个指向这个路由的链接
+    meta: {
+      title: "控制台",
+    },
+    component: () => import("../layout/index.vue"),
+  },
+
+  // News
+  {
+    path: "/news",
+    name: "News",
+    meta: {
+      title: "信息管理",
+    },
+    component: () => import("../layout/index.vue"),
+  },
+
+  // User
+  {
+    path: "/user",
+    name: "User",
+    meta: {
+      title: "用户管理",
+    },
     component: () => import("../layout/index.vue"),
   },
 
