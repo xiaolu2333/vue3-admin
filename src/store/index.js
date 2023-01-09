92c6a64895283d8c0d87f3e759cdcac6b2217e98
xiaolu2333 <2694977226@qq.com>
2023-01-09 17:37:14 +0800

    应用全局状态管理：状态管理基础之模块化 state。

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,20 @@
 /* jshint esversion: 6 */
 
+// // 创建一个 store 实例
+// export default createStore({
+//   state: {}, // 在这里放置所有的状态
+//   getters: {},
+//   mutations: {},
+//   actions: {},
+//   modules: {},
+// });
+
 import { createStore } from "vuex";
 
+import app from "./modules/app";
 export default createStore({
-  state: {},
-  getters: {},
-  mutations: {},
-  actions: {},
-  modules: {},
+  // 有很多 state 时可选择模块化 state
+  modules: {
+    app,
+  },
 });
---
+++ src/store/modules/app.js
@@ -0,0 +1,16 @@
+/* jshint esversion: 6 */
+
+const state = {
+  count: 100,
+};
+const getters = {};
+const mutations = {};
+const actions = {};
+
+export default {
+  namespaced: true, // 开启命名空间，防止命名冲突
+  state,
+  getters,
+  mutations,
+  actions,
+};
