57a96fbcef659768dcdfab9dd4aa9ef544d823c6
xiaolu2333 <2694977226@qq.com>
2023-01-10 15:47:17 +0800

    应用全局状态管理：状态管理基础之 Actions 提交 Mutations.
    Action 类似于 mutation，不同在于：
    - Action 提交的是 mutation，而不是直接变更状态。
    - Action 中的状态修改函数通常是异步的（）。
    [vuex.action](https://vuex.vuejs.org/zh/guide/actions.html#action).

--- src/store/modules/app.js
+++ src/store/modules/app.js
@@ -27,7 +27,20 @@ const mutations = {
     state.text = "Hello World!";
   },
 };
-const actions = {};
+const actions = {
+  // 3，通过参数解构的方式获取 commit 方法
+  update_count({ commit }, payload) {
+    // 4，可以在需要调用 commit 很多次的时候，简化代码
+    commit("SET_COUNT", payload);
+  },
+
+  // 1，接受一个与 store 实例具有相同方法和属性的 context 对象，以及一个可选的 payload
+  update_text(context, payload) {
+    console.log(context);
+    // 2，因此可以调用 context.commit 提交一个 mutation 来间接修改状态，或者通过 context.state 和 context.getters 来获取 state 和 getters。
+    context.commit("SET_TEXT");
+  },
+};
 
 export default {
   namespaced: true, // 开启命名空间，防止命名冲突
--- src/views/console/Index.vue
+++ src/views/console/Index.vue
@@ -22,7 +22,8 @@ export default {
     console.log("原始状态：", store.state.app.count);
     store.commit("app/SET_COUNT", 0); // 提交 mutation 中的状态修改动作来修改状态。
     console.log("更新后的状态：", store.state.app.count);
-
+    store.dispatch("app/update_count", 1); // 通过 action 中的状态修改动作需要通过 store.dispatch 方法触发（有模块化的，还必须指定模块名称）
+    console.log("新状态：", store.state.app.count);
     return {
       icon,
     };
