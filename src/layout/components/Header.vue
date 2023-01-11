31d389b9ec0308842f63b893c3cf949295da9814
xiaolu2333 <2694977226@qq.com>
2023-01-11 08:49:46 +0800

    应用全局状态管理：管理后台头部的制作之添加菜单栏按钮以及用户头像区域。
    后续打算实现通过点击菜单栏按钮来展开与收起侧边栏菜单。

--- src/layout/components/Header.vue
+++ src/layout/components/Header.vue
@@ -1,9 +1,38 @@
-<template></template>
+<template>
+  <div class="header-wrap">
+    <div class="wrap">
+      <span class="menu-btn">
+        <!--iconName是 svg 文件名，className是 svg 样式类名-->
+        <svg-icon iconName="menuBtn" className="icon-menu-svg"></svg-icon>
+      </span>
+    </div>
+    <div class="wrap">用户头像</div>
+  </div>
+</template>
 
 <script>
+import SvgIcon from "@/components/svgIcon/Index.vue";
+
 export default {
   name: "LayoutHeader",
+  components: { SvgIcon },
 };
 </script>
 
-<style scoped></style>
+<style lang="scss" scoped>
+.header-wrap {
+  height: 100%;
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+
+.menu-btn {
+  cursor: pointer;
+}
+
+.icon-menu-svg {
+  font-size: 24px;
+}
+</style>
