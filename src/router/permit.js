fbfeab07db3643281d2b860aa9803636aecbda80
xiaolu2333 <2694977226@qq.com>
2023-01-12 13:56:27 +0800

    路由导航守卫与登陆业务：校验 token 之获取 token 失败导致会“白屏”问题。

--- src/router/permit.js
+++ src/router/permit.js
@@ -2,9 +2,17 @@
 
 import router from "./index";
 
+import { getToken } from "@/utils/cookies";
+
 // 全局前置路由导航守卫
 router.beforeEach((to, from, next) => {
   console.log("全局前置路由导航守卫 to: ", to);
+  // 如果 token 不存在，则跳转到登录页
+  if (!getToken()) {
+    next("/login"); // 回报错，造成所谓白屏问题。
+    // [Vue Router warn]: Detected an infinite redirection in a navigation guard when going from "/" to "/login".
+    // Aborting to avoid a Stack Overflow. This will break in production if not fixed.
+  }
 });
 
 // 全局后值路由导航守卫
