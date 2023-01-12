172841ca3313e19d8407cc495670661097b10658
xiaolu2333 <2694977226@qq.com>
2023-01-12 08:36:20 +0800

    路由导航守卫与登陆业务：使用 cookie 存储数据之封装 cookie 操作。
    1，cookie 是客户端解决方案。
    2，cookie 由服务器端产生并返回给客户端，客户端以键值对方式将其存放在本地文件，并在后续的请求中携带它，用以用户身份验证。
    3，但 cookie 存在性能问题以及不易本地存储的问题，后者可以通过第三方包如js-cookie来解决。

---
+++ src/utils/cookies.js
@@ -0,0 +1,32 @@
+/* jshint esversion: 6 */
+
+// js-cookie 是一个轻量级的操作 cookie 的插件，它可以方便的设置、获取、删除 cookie。
+import Cookies from "js-cookie";
+
+const tokenKey = "tokenAdmin";
+// 设置 token
+export function setToken(token) {
+  return Cookies.set(tokenKey, token);
+}
+// 获取 token
+export function getToken() {
+  return Cookies.get(tokenKey);
+}
+// 移除 token
+export function removeToken() {
+  return Cookies.remove(tokenKey);
+}
+
+const userNameKey = "username";
+// 设置 userName
+export function setUserName(userName) {
+  return Cookies.set(userNameKey, userName);
+}
+// 获取 userName
+export function getUserName() {
+  return Cookies.get(userNameKey);
+}
+// 移除 userName
+export function removeUserName() {
+  return Cookies.remove(userNameKey);
+}
