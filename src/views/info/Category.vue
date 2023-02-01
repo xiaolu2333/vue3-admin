fc8804e0edfc55b5e5a2be15e0991690fbe4293d
xiaolu2333 <2694977226@qq.com>
2023-02-01 16:37:00 +0800

    信息管理模块：实现分类页之创建分类管理的布局。

--- src/views/info/Category.vue
+++ src/views/info/Category.vue
@@ -1,5 +1,10 @@
 <template>
-  <div>info category</div>
+  <el-button type="primary">添加一级分类</el-button>
+  <hr class="spacing-hr">
+  <el-row>
+    <el-col :span="10">树形菜单</el-col>
+    <el-col :span="14">输入区</el-col>
+  </el-row>
 </template>
 
 <script>
@@ -8,4 +13,10 @@ export default {
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.spacing-hr {
+  border: none;
+  border-top: 1px solid #e9e9e9;
+  margin: 10px 0;
+}
+</style>
