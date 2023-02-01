ece5e4dbb8683919660c2f8ed69851fed4307b8d
xiaolu2333 <2694977226@qq.com>
2023-02-01 09:06:39 +0800

    信息管理模块：实现信息列表页之构建列表页布局。

--- src/views/info/Index.vue
+++ src/views/info/Index.vue
@@ -1,5 +1,10 @@
 <template>
-  <div>info index</div>
+  <el-row>
+    <el-col :span="18">筛选区</el-col>
+    <el-col :span="6">
+      <el-button type="danger" class="pull-right">新增</el-button>
+    </el-col>
+  </el-row>
 </template>
 
 <script>
