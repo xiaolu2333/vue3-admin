a07a1ebeb9c1c50b75870a7faddd53dddd146293
xiaolu2333 <2694977226@qq.com>
2023-02-01 10:10:08 +0800

    信息管理模块：实现信息列表页之为搜索区提供部分模拟数据。
    在没有实现真正的接口之前，一般直接在组件中提供模拟数据。待接口开发后可使用 mock 技术模拟后端接口。
    
    el-select 的 v-model 绑定的数据类型必须与 el-option 的 :value 的值的属性一致。

--- src/views/info/Index.vue
+++ src/views/info/Index.vue
@@ -3,7 +3,19 @@
     <el-col :span="18">
       <el-form :inline="true" label-width="80px">
         <el-form-item label="类别:">
-          <el-select placehoder="请选择" class="width-160"></el-select>
+          <el-select
+            v-model="data.category"
+            placehoder="请选择"
+            class="width-160"
+          >
+            <el-option
+              v-for="item in data.category_options"
+              :key="item.value"
+              :label="item.label"
+              :value="item.value"
+            >
+            </el-option>
+          </el-select>
         </el-form-item>
         <el-form-item label="关键字:">
           <el-select placeholder="请选择" class="width-100"></el-select>
@@ -23,8 +35,77 @@
 </template>
 
 <script>
+import { reactive } from "vue";
+
 export default {
   name: "InfoIndex",
+  setup(props, { root }) {
+    // 模拟接口数据
+    const data = reactive({
+      category: 0, // 默认选项
+      category_options: [
+        {
+          label: "人工智能",
+          value: 0,
+        },
+        {
+          label: "区块链",
+          value: 1,
+        },
+        {
+          label: "云计算",
+          value: 2,
+        },
+        {
+          label: "大数据",
+          value: 3,
+        },
+        {
+          label: "物联网",
+          value: 4,
+        },
+        {
+          label: "移动开发",
+          value: 5,
+        },
+        {
+          label: "前端开发",
+          value: 6,
+        },
+        {
+          label: "后端开发",
+          value: 7,
+        },
+        {
+          label: "数据库",
+          value: 8,
+        },
+        {
+          label: "运维",
+          value: 9,
+        },
+        {
+          label: "测试",
+          value: 10,
+        },
+        {
+          label: "UI设计",
+          value: 11,
+        },
+        {
+          label: "游戏开发",
+          value: 12,
+        },
+        {
+          label: "其他",
+          value: 13,
+        },
+      ],
+    });
+    return {
+      data,
+    };
+  },
 };
 </script>
 
@@ -32,9 +113,11 @@ export default {
 .width-160 {
   width: 160px;
 }
+
 .width-100 {
   width: 100px;
 }
+
 .width-180 {
   width: 180px;
 }
