abff9939aeb5980b1878c93763320250cde6048a
xiaolu2333 <2694977226@qq.com>
2023-02-01 08:52:04 +0800

    信息管理模块：实现数据展示区UI稿件效果。

--- src/layout/components/Main.vue
+++ src/layout/components/Main.vue
@@ -10,4 +10,13 @@ export default {
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+#main-content {
+  background-color: #fff;
+  padding: 20px;
+  /* 使内容区域的高度自适应父元素高度 */
+  min-height: 100%;
+  /* 忽略 padding 和 border 带来的 padding、border、width、height 叠加的高度宽度影响 */
+  @include webkit(box-sizing, border-box);
+}
+</style>
